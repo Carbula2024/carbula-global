c6f13fea47bcdb3627fc1a47d90fd48e71e71346
Carbula2024 <daniel@carbula.com>
2024-11-06 15:10:03 -0400

    add title cotizer

--- components/Widget/Widget.jsx
+++ components/Widget/Widget.jsx
@@ -1,6 +1,8 @@
+import styles from './widget.module.css'
 const Widget = () =>{
     return(
         <>
+        <h3 className={styles.title_cotizer}>Cotiza tu vehículo</h3>
         <div style={{width: '100%', height: '100%'}} id="autored-frame"></div>
         <script type="text/javascript" src="https://embeddable.autored.cl/loader.js"></script>
         </>
