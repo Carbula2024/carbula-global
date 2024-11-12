190c3cd554dbfe40e91949f203b29b8621634c16
Carbula2024 <daniel@carbula.com>
2024-11-12 13:59:12 -0400

    features - addtiktokpixelapi

--- pages/api/tiktok-view-page.js
+++ pages/api/tiktok-view-page.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
               body: JSON.stringify(
               {
                 pixel_code: tiktokpixelID,
-                event: "View Page",
+                event: "Page View",
                 context: {
                   page: {
                     url: url,
