9fa9def9637bbe668610d7db99a99a1643daed1f
Carbula2024 <daniel@carbula.com>
2024-11-12 15:32:00 -0400

    edit PageView tktkpxel

--- pages/api/tiktok-view-page.js
+++ pages/api/tiktok-view-page.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
               body: JSON.stringify(
               {
                 pixel_code: tiktokPixelId,
-                event: "Pageview",
+                event: "PageView",
                 context: {
                   page: {
                     url: url,
