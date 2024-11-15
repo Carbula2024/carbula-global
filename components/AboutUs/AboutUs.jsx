beaa074087d94dfa834494a8f2752479aa024c27
Carbula2024 <daniel@carbula.com>
2024-11-15 17:03:27 -0400

    add icons and text about me

--- components/AboutUs/AboutUs.jsx
+++ components/AboutUs/AboutUs.jsx
@@ -4,18 +4,11 @@ const AboutUs = () => {
     return (
         <>
             <div className={styles.aboutus_container}>
-            <h3>Sobre nosotros</h3>
+                <h3>Sobre nosotros</h3>
                 <div className={styles.presentation_container}>
                     <div className={styles.presentation_text}>
-                    <h3>Quienes somos</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Libero natus quae maxime labore voluptate
-                            corporis, ipsa impedit! Quos, officiis iste harum
-                            saepe quae nisi suscipit dolores quod fugit rerum
-                            ipsam officia magnam obcaecati eum assumenda?
-                            Aperiam, dolore. Cumque modi libero illum quibusdam
-                            culpa rem voluptatem dolores repellendus, odio
-                            tempora alias!</p>
+                        <h3>Quienes somos</h3>
+                        <p>En Cárbula, estamos comprometidos con la compraventa de vehículos en consignación, facilitando todo el proceso desde la comodidad de tu hogar. Nuestro equipo de expertos se dedica a ofrecer un servicio transparente, seguro y eficiente, conectando a vendedores y compradores de manera confiable. Con Cárbula, puedes gestionar la venta o compra de tu vehículo sin complicaciones, aprovechando nuestra plataforma innovadora y el soporte personalizado de nuestros profesionales. ¡Conócenos y descubre cómo hacemos que la experiencia de adquirir o vender tu vehículo sea sencilla y satisfactoria!</p>
                     </div>
                     <video width="100%" controls className={styles.presentation_video}>
                         <source src='/videos/presentacion.mp4' type="" />
@@ -24,19 +17,19 @@ const AboutUs = () => {
                 </div>
                 <div className={styles.contact_us}>
                     <div className={styles.img_container}>
-                    <img src="/images/aboutus/Daniel.png" alt="" className={styles.img_us} />
-                    <p>Teléfono: <a href="tel:+1234567890">+1 234 567 890</a></p>
-                    <p>Email: <a href="mailto:contacto1@example.com">correo@carbula.com</a></p>
+                        <img src="/images/aboutus/Daniel.png" alt="" className={styles.img_us} />
+                        <p className={styles.phone}><img src="/images/aboutus/phone.svg" alt="" /><a href="">+1 234 567 890</a></p>
+                        <p className={styles.email}><img src="/images/aboutus/email.svg" alt="" /><a href="">correo@carbula.com</a></p>
                     </div>
                     <div className={styles.img_container}>
-                    <img src="/images/aboutus/Natasha.png" alt="" className={styles.img_us} />
-                    <p>Teléfono: <a href="tel:+1234567890">+1 234 567 890</a></p>
-                    <p>Email: <a href="mailto:contacto1@example.com">correo@carbula.com</a></p>
+                        <img src="/images/aboutus/Natasha.png" alt="" className={styles.img_us} />
+                        <p className={styles.phone}><img src="/images/aboutus/phone.svg" alt="" /><a href="">+1 234 567 890</a></p>
+                        <p className={styles.email}><img src="/images/aboutus/email.svg" alt="" /><a href="">correo@carbula.com</a></p>
                     </div>
                     <div className={styles.img_container}>
-                    <img src="/images/aboutus/Marcelo.png" alt="" className={styles.img_us} />
-                    <p>Teléfono: <a href="tel:+1234567890">+1 234 567 890</a></p>
-                    <p>Email: <a href="mailto:contacto1@example.com">correo@carbula.com</a></p>
+                        <img src="/images/aboutus/Marcelo.png" alt="" className={styles.img_us} />
+                        <p className={styles.phone}><img src="/images/aboutus/phone.svg" alt="" /><a href="">+1 234 567 890</a></p>
+                        <p className={styles.email}><img src="/images/aboutus/email.svg" alt="" /><a href="">correo@carbula.com</a></p>
                     </div>
                 </div>
             </div>
