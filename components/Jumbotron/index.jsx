68806fe11a7850e0bedaaa4af02243e1caf3d7ef
Carbula2024 <daniel@carbula.com>
2024-11-18 18:44:03 -0400

    change-banner and delete text

--- components/Jumbotron/index.jsx
+++ components/Jumbotron/index.jsx
@@ -4,10 +4,6 @@ import styles from './jumbotron.module.scss';
 const Jumbotron = ({title, subtitle}) => {
   return (
     <div className={styles.jumbotron__container}>
-      <div className={styles.text__container}>
-        <h2 className={styles.title}>{title[0]}<br/>{title[1]}</h2>
-        <p className={styles.subtitle}><ul>{subtitle.map(element=><Fragment><li>{element}</li></Fragment>)}</ul></p>
-      </div>
 
     </div>
   )
