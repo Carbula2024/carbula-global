ac7e8ed2f99c9b3d3be3ba36efa3fb342c81cd0f
Carbula2024 <daniel@carbula.com>
2024-12-24 18:59:17 -0400

    clean widget

--- components/Widget/Widget.jsx
+++ components/Widget/Widget.jsx
@@ -1,28 +1,6 @@
 import styles from './widget.module.css'
 import { useEffect } from 'react';
 const Widget = () =>{
-    useEffect(() => {
-        const nextButton = document.getElementById('autored-tab-3'); // ID del botón "Siguiente"
-    
-        if (nextButton) {
-          nextButton.addEventListener('click', () => {
-            gtag_report_conversion();
-            const script = document.createElement('script');
-            script.src = 'https://embeddable.autored.cl/loader.js';
-            script.async = true;
-            document.body.appendChild(script);
-          });
-        }
-    
-        // Limpieza: remueve el evento cuando el componente se desmonta
-        return () => {
-          if (nextButton) {
-            nextButton.removeEventListener('click', () => {
-            });
-          }
-        };
-      }, []); // Solo se ejecuta una vez cuando el componente se monta
-    
     return(
         <>
         <h3 className={styles.title_cotizer}>Cotiza tu auto</h3>
