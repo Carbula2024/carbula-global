dad6590f292ddb69576d46a9021b5f77bc4c32ee
Carbula2024 <daniel@carbula.com>
2024-12-24 18:54:52 -0400

    change color nav, cut contact us natasha

--- components/AboutUs/AboutUs.jsx
+++ components/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ const AboutUs = () => {
             <div className={styles.aboutus_container} id='sobre-nosotros'>
                 <div className={styles.presentation_container}>
                     <div className={styles.presentation_text}>
-                        <span>Sobre Carbula</span>
+                        <span>Sobre Cábula</span>
                         <h3>Conectamos compradores y vendedores <b>en un entorno seguro y eficiente.</b> </h3>
                         <p>En Cárbula, te ofrecemos soluciones personalizadas para comprar y vender autos desde la comodidad de tu hogar. Podemos comprar tu auto de forma directa o ponértelo en consignación para obtener el mejor valor posible. Nuestro equipo profesional se encarga de todo, desde la evaluación hasta la negociación. También ofrecemos financiamiento y la opción de recibir tu auto como parte de pago.</p>
                     </div>
@@ -31,14 +31,8 @@ const AboutUs = () => {
                         <p className={styles.email}><img src="/images/aboutus/email.svg" alt="email" />daniel@carbula.com</p>
                     </div>
                     <div className={styles.img_container}>
-                        <img src="/images/aboutus/Natasha.png" alt="Miembro del equipo" className={styles.img_us} />
-                        <h3>Gerente marketing</h3>
-                        <p className={styles.phone}><img src="/images/aboutus/phone.svg" alt="phone" />+56935317622</p>
-                        <p className={styles.email}><img src="/images/aboutus/email.svg" alt="email" />natasha@carbula.com</p>
-                    </div>
-                    <div className={styles.img_container}>
                         <img src="/images/aboutus/Marcelo.png" alt="Miembro del equipo" className={styles.img_us} />
-                        <h3>Gerente operaciones</h3>
+                        <h3>Gerente operaciones</h3>
                         <p className={styles.phone}><img src="/images/aboutus/phone.svg" alt="phone" />+56971550647</p>
                         <p className={styles.email}><img src="/images/aboutus/email.svg" alt="email" />marcelo@carbula.com</p>
                     </div>
