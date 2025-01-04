f07b5df215a5b0d5c7577249c9a513930db6b868
Carbula2024 <daniel@carbula.com>
2025-01-04 16:59:43 -0400

    change text jumbotron

--- components/Jumbotron/index.jsx
+++ components/Jumbotron/index.jsx
@@ -1,12 +1,19 @@
 import { Fragment } from 'react';
 import styles from './jumbotron.module.scss';
 
+const newSubtitle = [
+  "Establecidos en 2018, Cárbula ha vendido sobre 1000 autos.",
+  "Transferimos autos a través de Autofact de manera 100% digital y segura.",
+  "Tasamos y verificamos el estado y kilometraje de cada auto con Autored.",
+  "Recibimos tu auto en parte de pago, sujeto a evaluación."
+];
+
 const Jumbotron = ({ title, subtitle, className }) => {
   return (
     <div className={`${styles.jumbotron__container} ${className}`}>
       <div className={styles.text__container}>
         <h2 className={styles.title}>{title[0]}<br />{title[1]}</h2>
-        <p className={styles.subtitle}><ul className={styles.list_jumbotron}>{subtitle.map(element => <Fragment><li><img src="./../icons/check.svg" alt="" />{element}</li></Fragment>)}</ul></p>
+        <p className={styles.subtitle}><ul className={styles.list_jumbotron}>{newSubtitle.map(element => <Fragment><li><img src="./../icons/check.svg" alt="" />{element}</li></Fragment>)}</ul></p>
       </div>
       <img className={styles.banner_desktop} src="/images/banner/desktop.png" alt="" />
       <img className={styles.banner_mobile} src="/images/banner/mobile.png" alt="" />
