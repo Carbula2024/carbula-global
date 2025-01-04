95847e5c81b654ddaded34821f3deeaf3c6511cf
Carbula2024 <daniel@carbula.com>
2025-01-04 15:53:36 -0400

    remove desde 2018

--- components/AboutUs/AboutUs.jsx
+++ components/AboutUs/AboutUs.jsx
@@ -10,8 +10,6 @@ const AboutUs = () => {
                         <h3>Conectamos compradores y vendedores <b>en un entorno seguro y eficiente.</b> </h3>
                         <p>En Cárbula, te ofrecemos soluciones personalizadas para comprar y vender autos desde la comodidad de tu hogar. Podemos comprar tu auto de forma directa o ponértelo en consignación para obtener el mejor valor posible. Nuestro equipo profesional se encarga de todo, desde la evaluación hasta la negociación. También ofrecemos financiamiento y la opción de recibir tu auto como parte de pago.
                             <br /><br />
-                            <span>Trabajando para nuestros clientes desde 2018.</span>
-                            <br />
                             <span>Agenda una visita a nuestra nueva oficina en Las Condes.</span>
                         </p>
                     </div>
