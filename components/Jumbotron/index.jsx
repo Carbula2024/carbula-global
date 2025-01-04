c31f4a81a43e1ed5d34d0e78427110ed891325b5
Carbula2024 <daniel@carbula.com>
2025-01-04 15:08:04 -0400

    add check list jumbotron

--- components/Jumbotron/index.jsx
+++ components/Jumbotron/index.jsx
@@ -6,7 +6,7 @@ const Jumbotron = ({ title, subtitle, className }) => {
     <div className={`${styles.jumbotron__container} ${className}`}>
       <div className={styles.text__container}>
         <h2 className={styles.title}>{title[0]}<br />{title[1]}</h2>
-        <p className={styles.subtitle}><ul>{subtitle.map(element => <Fragment><li>{element}</li></Fragment>)}</ul></p>
+        <p className={styles.subtitle}><ul className={styles.list_jumbotron}>{subtitle.map(element => <Fragment><li><img src="./../icons/check.svg" alt="" />{element}</li></Fragment>)}</ul></p>
       </div>
       <img className={styles.banner_desktop} src="/images/banner/desktop.png" alt="" />
       <img className={styles.banner_mobile} src="/images/banner/mobile.png" alt="" />
