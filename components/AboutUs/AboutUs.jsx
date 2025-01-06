90cfd71d693d3050dec638ae558b949c76c0b533
Carbula2024 <daniel@carbula.com>
2025-01-05 20:21:30 -0400

    change text banner mobile

--- components/AboutUs/AboutUs.jsx
+++ components/AboutUs/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
                 <div className={styles.presentation_container}>
                     <div className={styles.presentation_text}>
                         <span>Sobre Cábula</span>
-                        <h3>Conectamos compradores y vendedores <b>en un entorno seguro y eficiente.</b> </h3>
+                        <h3>Conectamos vendedores y compradores desde 2018, con +1000 autos vendidos exitosamente!</h3>
                         <p>En Cárbula, te ofrecemos soluciones personalizadas para comprar y vender autos desde la comodidad de tu hogar. Podemos comprar tu auto de forma directa o ponértelo en consignación para obtener el mejor valor posible. Nuestro equipo profesional se encarga de todo, desde la evaluación hasta la negociación. También ofrecemos financiamiento y la opción de recibir tu auto como parte de pago.
                             <br /><br />
                             <span>Agenda una visita a nuestra nueva oficina en Las Condes.</span>
--- components/Jumbotron/index.jsx
+++ components/Jumbotron/index.jsx
@@ -13,7 +13,14 @@ const Jumbotron = ({ title, subtitle, className }) => {
     <div className={`${styles.jumbotron__container} ${className}`}>
       <div className={styles.text__container}>
         <h2 className={styles.title}>{title[0]}<br />{title[1]}</h2>
+        <h2 className={styles.title_mobile}>Compra o vende tu auto con Cárbula! </h2>
         <p className={styles.subtitle}><ul className={styles.list_jumbotron}>{newSubtitle.map(element => <Fragment><li><img src="./../icons/check.svg" alt="" />{element}</li></Fragment>)}</ul></p>
+        <p className={styles.subtitle_mobile}>
+          <ul className={styles.list_jumbotron_mobile}>
+            <li><a href="#autored-tab-content">Quiero vender mi auto</a></li>
+            <li><a href="https://listado.mercadolibre.cl/_Tienda_carbula">Busco mi próximo auto</a></li>
+          </ul>
+          </p>
       </div>
       <img className={styles.banner_desktop} src="/images/banner/desktop.png" alt="" />
       <img className={styles.banner_mobile} src="/images/banner/mobile.png" alt="" />
